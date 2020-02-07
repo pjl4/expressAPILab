05e07fdf019ec2bbc453d432d0fbb67a7cd672fc
pjl4 <pj.lutz17@gmail.com>
2020-02-07 11:10:34 -0500

    added seed data

---
+++ backend/db/seeds.js
@@ -0,0 +1,30 @@
+[
+	{
+		name: 'Beyonce',
+		url: 'https://gph.is/1MPYZqc'
+	},
+	{
+		name: 'Jonah hill excited',
+		url: 'https://gph.is/15RTH5O'
+	},
+	{
+		name: 'Thumbs up kid',
+		url: 'https://gph.is/g/4691918'
+	},
+	{
+		name: 'Gritty',
+		url: 'https://gph.is/2pxP7Zl'
+	},
+	{
+		name: 'TWICE!',
+		url: 'https://gph.is/g/aKNQ1Wl'
+	},
+	{
+		name: 'Gatorade',
+		url: 'https://gph.is/1sCimKa'
+	},
+	{
+		name: 'Coding cat',
+		url: 'https://gph.is/2nm7ouh'
+	}
+];
